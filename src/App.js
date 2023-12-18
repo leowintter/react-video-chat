565d43c1c33ce79805bf41f1073069290b8b913f
Leonardo Winter <leowintter13@gmail.com>
2023-12-18 14:39:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useRef } from "react";
+import SimplePeer from "simple-peer";
 
 function App() {
+  const myVideoRef = useRef();
+  const peerVideoRef = useRef();
+  let peer;
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        myVideoRef.current.srcObject = stream;
+
+        peer = new SimplePeer({
+          initiator: window.location.hash === "#init",
+          trickle: false,
+          stream,
+        });
+
+        peer.on("signal", (data) => {
+          // Enviar o objeto 'data' para o outro participante
+        });
+
+        peer.on("stream", (stream) => {
+          peerVideoRef.current.srcObject = stream;
+        });
+      })
+      .catch((error) =>
+        console.error("Erro ao acessar câmera/microfone:", error)
+      );
+
+    return () => {
+      peer && peer.destroy();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>teste</p>
+      <video ref={myVideoRef} autoPlay muted style={{ width: "50%" }} />
+      <video ref={peerVideoRef} autoPlay style={{ width: "50%" }} />
     </div>
   );
 }
